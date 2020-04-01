4da7e986730ee56be78fe148ef319982c38d2cfc
Gula Sebastian <gulasebastian99@gmail.com>
2020-04-01 19:03:19 +0300

    [ADD]

---
+++ Farmacie/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Librarie;
+
+
+namespace Farmacie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuare = true;
+
+            Pharmacy med1 = new Pharmacy("Parasinus", 12, 1, "Catena");
+            Pharmacy med2 = new Pharmacy("Decasept", 10, 1, "HelpNet");
+            Pharmacy med3 = new Pharmacy("Nurofen,15,1,SensiBlue");
+            Pharmacy med4 = new Pharmacy();
+            Pharmacy med5 = new Pharmacy("Decasept", 9, 1, "Catena");
+            Pharmacy med6 = new Pharmacy("Tusocalm", 15, 0, "UrbanGinaPharm");
+
+            string s1, s2, s3, s4, s5, s6;
+
+
+
+            while (continuare)
+            {
+                Console.Clear();
+                Console.WriteLine("                          PHARMALIFE-APLICATIE MOBILA\n              ");
+                Console.WriteLine("A.Adaugare medicament in baza de date.");
+                Console.WriteLine("I.Info Medicamente(Afisare)");
+                Console.WriteLine("V.Verificare valabilitate medicamente.");
+                Console.WriteLine("L.Verificati locatiile unde se gasesc medicamentele.");
+                Console.WriteLine("C.Comparare pret ");
+                Console.WriteLine("E.Exit.");
+                Console.WriteLine("Alegeti o optiune: ");
+
+                string opt = Console.ReadLine();
+
+
+
+                switch (opt.ToUpper())
+                {
+                    case "A":
+                        {
+                            med4 = citireTastatura();
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "I":
+                        {
+                            s1 = med1.Info();
+                            Console.WriteLine(s1);
+                            s2 = med2.Info();
+                            Console.WriteLine(s2);
+                            s3 = med3.Info();
+                            Console.WriteLine(s3);
+                            s4 = med4.Info();
+                            Console.WriteLine(s4);
+                            s5 = med5.Info();
+                            Console.WriteLine(s5);
+                            s6 = med6.Info();
+                            Console.WriteLine(s6);
+
+                            Console.WriteLine("Nu s-a adaugat niciun alt medicament.\n");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "V":
+                        {
+                            s1 = med1.Valabil();
+                            Console.WriteLine(s1);
+                            s2 = med2.Valabil();
+                            Console.WriteLine(s2);
+                            s3 = med3.Valabil();
+                            Console.WriteLine(s3);
+                            s4 = med4.Valabil();
+                            Console.WriteLine(s4);
+                            s5 = med5.Valabil();
+                            Console.WriteLine(s5);
+                            s6 = med6.Valabil();
+                            Console.WriteLine(s6);
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "L":
+                        {
+                            s1 = med1.map();
+                            Console.WriteLine(s1);
+                            s2 = med2.map();
+                            Console.WriteLine(s2);
+                            s3 = med3.map();
+                            Console.WriteLine(s3);
+                            s4 = med4.map();
+                            Console.WriteLine(s4);
+                            s5 = med5.map();
+                            Console.WriteLine(s5);
+                            s6 = med6.map();
+                            Console.WriteLine(s6);
+
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "C":
+                        {
+                            Console.WriteLine("Intrucat medicamentul Decasept se gaseste in doua farmacii, se va compara pretul spre a se vedea care locatie este mai rentabila.");
+
+                            if (med2.compararePret(med5) == Pharmacy.higher)
+                                Console.WriteLine(" Pretul este mai avantajos in farmacia {0} ", med5.locatie);
+                            else if (med2.compararePret(med5) == Pharmacy.less)
+                                Console.WriteLine("Pretul este mai avantajos in farmacia {0}", med2.locatie);
+                            else
+                                Console.WriteLine("Pretul este egal.");
+
+
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "E":
+                        {
+                            continuare = false;
+
+                            break;
+
+                        }
+                }
+            }
+
+        }
+
+        public static Pharmacy citireTastatura()
+        {
+            Console.WriteLine("Introduceti numele medicamentului pe care doriti sa-l inserati in baza de date:");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti pretul:");
+            int pret = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti daca este valabil momentan[1-valabil 0-nevalabil]:");
+            int valabilitate = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti locatia unde se poate gasi:");
+            string locatie = Console.ReadLine();
+
+
+            Console.WriteLine("Va rugam sa verificicati din nou informatiile despre medicamente.");
+
+            Pharmacy m = new Pharmacy(nume, pret, valabilitate, locatie);
+
+            return m;
+        }
+
+
+    }
+}
---
+++ Librarie/Pharmacy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarie
+{
+    public class Pharmacy
+    {
+        //constante
+        public const int higher = 1;
+        public const int equal = 0;
+        public const int less = 1;
+
+        public string nume { get; set; }
+        public int pret { get; set; }
+        public int valabilitate { get; set; }
+        public string locatie { get; set; }
+
+        //constructor fara parametrii
+        public Pharmacy()
+        {
+            nume = string.Empty;
+            locatie = string.Empty;
+            pret = 0;
+            valabilitate = 0;
+
+        }
+
+        //constructor cu parametrii
+        public Pharmacy(string denumire, int cost, int valabil, string loc)
+        {
+            nume = denumire;
+            pret = cost;
+            valabilitate = valabil;
+            locatie = loc;
+
+        }
+
+        public Pharmacy(string nume1)
+        {
+            string[] buff = nume1.Split(',');
+            nume = buff[0];
+            pret = Int32.Parse(buff[1]);
+            valabilitate = Int32.Parse(buff[2]);
+            locatie = buff[3];
+        }
+        public string Info()
+        {
+            return string.Format("Medicamentul este {0} si are pretul de {1} lei", nume, pret);
+
+        }
+
+        public string Valabil()
+        {
+            if (valabilitate == 1)
+                return string.Format("Medicamentul {0} este valabil.", nume);
+            else
+                return string.Format("Medicamentul {0} nu este valabil", nume);
+
+        }
+        public string map()
+        {
+            return string.Format("Medicamentul {0} se gaseste la farmacia {1}. ", nume, locatie);
+        }
+
+        public int compararePret(Pharmacy _p)
+        {
+            return this.pret.CompareTo(_p.pret);
+        }
+
+    }
+
+}
