effd95677cd2d92abedbbeec523148682da90915
Gula Sebastian <gulasebastian99@gmail.com>
2020-04-02 21:18:53 +0300

    [ADD]-FINAL

--- Farmacie/Program.cs
+++ Farmacie/Program.cs
@@ -122,8 +122,6 @@ namespace Farmacie
 
             return m;
         }
-       
-
-
+        
     }
 }
---
+++ Librarie/Enumerare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarie
+{
+    public enum Grup
+        {
+        Capsula = 1,
+        Sirop = 2,
+        Injectie = 3,
+        Unguent = 4,
+        UNKNOWN = 5,
+    }
+}
